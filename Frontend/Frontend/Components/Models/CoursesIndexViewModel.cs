f165ea455d738d056843d55efafebcd8239bd25f
Hampusholmberg <117671157+Hampusholmberg@users.noreply.github.com>
2024-05-29 10:28:41 +0200

    code cleanup

--- Frontend/Frontend/Components/Models/CoursesIndexViewModel.cs
+++ Frontend/Frontend/Components/Models/CoursesIndexViewModel.cs
@@ -1,15 +1,7 @@
-﻿//using Infrastructure.Models;
-
-//using Infrastructure.Models;
-namespace Frontend.Components.Models;
+﻿namespace Frontend.Components.Models;
 
 public class CoursesIndexViewModel
 {
     public string Title { get; set; } = "";
     public List<CourseModel> Courses { get; set; } = null!;
-    //public IEnumerable<CategoryViewModel> Categories { get; set; } = null!;
-    //public AccountViewModel Account { get; set; } = null!;
-    //public Pagination? Pagination { get; set; }
-    //public string? CourseCategory { get; set; }
-    //public string? SearchQuery { get; set; }
 }
